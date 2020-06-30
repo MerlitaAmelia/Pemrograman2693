22076d0b46bb21faf01499508f675f6b7d497b46
MerlitaAmelia <61940838+MerlitaAmelia@users.noreply.github.com>
2020-06-30 21:12:38 +0700

    Create KaryawanHarian.cs

---
+++ KaryawanHarian.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OOPdanCollection.ClassInduk;
+namespace AplikasiKaryawan2723.ClassAnak
+
+{
+    public class KaryawanHarian : Karyawan
+    {
+        public int UpahPerJam { get; set; }
+        public int JumlahJamKerja { get; set; }
+        
+        public override int Gaji() => UpahPerJam * JumlahJamKerja;
+
+
+    }
+}
